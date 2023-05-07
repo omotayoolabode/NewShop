a7cf84ee7d8e0431cba6739c757e3c72eb95bdec
Omotayo Olabode <omotayoolabodeo3@gmail.com>
2023-05-07 14:38:06 +0100

    Fixed Masterpage

---
+++ NewShop/Controllers/ShopController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using NewShop.Services.Interface;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace NewShop.Controllers
+{
+    public class ShopController : RenderController
+    {
+        private readonly IProductHelper _productHelper;
+       
+        public ShopController(ILogger<RenderController> logger, IProductHelper productHelper,ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor) : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+            _productHelper = productHelper;
+        }
+        public override IActionResult Index()
+        {
+            ViewBag.currentpageurl = CurrentPage.Url();
+            var products = _productHelper.Getallproduct();
+            ViewBag.allproducts = products;
+            return CurrentTemplate(CurrentPage);
+        }
+    }
+}
--- NewShop/Services/Implementation/ProductHelper.cs
+++ NewShop/Services/Implementation/ProductHelper.cs
@@ -3,26 +3,29 @@ using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Web.Common;
 using Umbraco.Cms.Web.Common.PublishedModels;
 
-namespace NewShop.Services.Implementation
-{
-    public class ProductHelper : IProductHelper
-    {
-        private readonly UmbracoHelper _helper;
-        public ProductHelper(UmbracoHelper helper)
-        {
-            _helper = helper;
-        }
-        public IEnumerable<Product> Getallproduct()
-        {
-            var allproduct = _helper.Content(Guid.Parse("f3fc9b58-6658-4000-ad0f-246041a3f830")).ChildrenOfType("product").Where(x => x.IsVisible());
-            var allproductasmodel = allproduct as IEnumerable<Product>;
-            return allproductasmodel;
-        }
+namespace NewShop.Services.Implementation;
 
-        public Product Getproductbyid(int id)
-        {
-            var product = Getallproduct().FirstOrDefault(x => x.Id == id) as Product;
-            return product;
-        }
+public class ProductHelper : IProductHelper
+{
+    private readonly UmbracoHelper _helper;
+    public ProductHelper(UmbracoHelper helper)
+    {
+        _helper = helper;
+    }
+
+        
+    public IEnumerable<Product> Getallproduct()
+    {
+        var allproduct = _helper.Content(Guid.Parse("f3fc9b58-6658-4000-ad0f-246041a3f830"))
+            .ChildrenOfType("product")
+            .Where(x => x.IsVisible());
+
+        return allproduct.Select(item => item as Product).ToList();
+    }
+
+    public Product Getproductbyid(int id)
+    {
+        var product = Getallproduct().FirstOrDefault(x => x.Id == id);
+        return product;
     }
 }
